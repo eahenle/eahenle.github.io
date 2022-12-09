6e1e282472171274376164fd3604372583679369
Adrian Henle <eahenle@gmail.com>
2022-12-08 16:19:45 -0800

    working

--- 49b2e87f-ec73-43b4-a103-a66600f11bd0/serve.jl
+++ 49b2e87f-ec73-43b4-a103-a66600f11bd0/serve.jl
@@ -1,11 +1,28 @@
 using HTTP
+
+function parse_file(filename)
+    return open(filename, "r") do f
+        return [UInt8(x) for x in read(f)]
+    end
+end
+
+function server(request)
+    if request.target == "/"
+        return HTTP.Response(200, parse_file("index.html"))
+    elseif request.target == "/data.csv"
+        return HTTP.Response(200, parse_file("data.csv"))
+    elseif request.target == "/favicon.ico"
+        return HTTP.Response(200)
+    end
+
+    tokens = split(request.target, "/img/")
+    if length(tokens) == 2
+        return HTTP.Response(200, parse_file("img/" * tokens[2]))
+    end
+
+    return HTTP.Response(400)
+end
+
 # start a server listening on port 8081 (default port) for localhost (default host)
 # hosts a single page (index.html from the current directory)
-HTTP.serve(
-    _ -> HTTP.Response(
-        200, 
-        open("index.html", "r") do f
-            return [UInt8(x) for x in read(f)]
-        end
-    )
-)
+HTTP.serve(server)
