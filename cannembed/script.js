a603d7fdc66deb6eb4c678a55aa3f39593b02dfd
Adrian Henle <eahenle@gmail.com>
2022-12-09 14:13:30 -0800

    plot adjustments

--- cannembed/script.js
+++ cannembed/script.js
@@ -9,7 +9,7 @@ d3.csv(
             index : d.index,
             x : +d.x,
             y : +d.y,
-            label : d.active
+            label : d.active // note: label is switched (active = 'false')
         }
     }, 
     function(error, rows) {
@@ -21,12 +21,12 @@ d3.csv(
 // generate interactive plot
 function createVisualization() {
     // image dimensions
-    var w = 700
-    var h = 700
+    var w = 1000
+    var h = 1000
     // axis padding
-    var padding = 70
+    var padding = 75
     // toolitp image box edge length
-    var image_size = 250
+    var image_size = 200
 
     // create the SVG
     var svg = d3.select("body")
@@ -35,24 +35,20 @@ function createVisualization() {
         .attr("height", h)
 
     // set plot scaling
-    var xMax = d3.max(molData, function(d) { return d.x })
-    var xMin = d3.min(molData, function(d) { return d.x })
+    var xMax = d3.max(molData, function(d) { return d.x }) + padding
+    var xMin = d3.min(molData, function(d) { return d.x }) - padding
     var xScale = d3.scaleLinear()
         .domain([xMin, xMax]) 
         .range([padding, w - padding]) 
 
-    var yMax = d3.max(molData, function(d) { return d.y })
-    var yMin = d3.min(molData, function(d) { return d.y })
+    var yMax = d3.max(molData, function(d) { return d.y }) + padding
     var yScale = d3.scaleLinear()
-        .domain([yMin, yMax])
+        .domain([yMax + xMin, yMax - xMax])
         .range([h - padding, padding])
 
     // create axes
-    var xAxis = d3.axisBottom(xScale)
-        .ticks(5)
-
-    var yAxis = d3.axisLeft(yScale)
-        .ticks(5)
+    var xAxis = d3.axisBottom(xScale).ticks(5)
+    var yAxis = d3.axisLeft(yScale).ticks(5)
 
     // append axes to plot SVG
     svg.append("g")
@@ -70,8 +66,8 @@ function createVisualization() {
         .attr("class", "y label")
         .attr("text-anchor", "middle")
         .text("Latent Dimension 2")
-        .attr("transform", "translate(10, " + (h / 2) + ") rotate (-90)")
-        .attr("font-size", "14")
+        .attr("transform", "translate(25, " + (h / 2) + ") rotate (-90)")
+        .attr("font-size", "18")
         .attr("font-family", "'Open Sans', sans-serif")
 
     svg.append("text")
@@ -79,16 +75,16 @@ function createVisualization() {
         .attr("text-anchor", "middle")
         .text("Latent Dimension 1")
         .attr("transform", "translate(" + (w / 2) + ", " + (h - 10) + ")")
-        .attr("font-size", "14")
+        .attr("font-size", "18")
         .attr("font-family", "'Open Sans', sans-serif")
 
     // Title
     svg.append("text")
         .attr("class", "Title")
         .attr("text-anchor", "middle")
-        .text("Latent Molecular Space")
+        .text("Molecular Embedding")
         .attr("transform", "translate(" + (w / 2) + ", " + (0.5 * padding) + ")")
-        .attr("font-size", "20")
+        .attr("font-size", "24")
         .attr("font-family", "'Open Sans', sans-serif")
 
     // Create circles from molData
@@ -99,32 +95,41 @@ function createVisualization() {
         .attr("cx", function(d) { return xScale(d.x) })
         .attr("cy", function(d) { return yScale(d.y) })
         .attr("r", 5)
-        .attr("fill", "steelblue")
-        .on("mouseover", function(d) {
-            tooltip.style("visibility", "visible")
-        })
-        .on("mousemove", function(d) {
-            var string = "<img src=img/" + d.index + ".png style='height: " + image_size + "px; width: " + image_size + "px; object-fit: contain'>"
-            return {
-                image : tooltip.html(string)
-                    .style("left", (d3.event.pageX + (d3.event.pageX > w / 2 ? -(10 + image_size) : 10)) + "px")
-                    .style("top", (d3.event.pageY + (d3.event.pageY > h / 2 ? -(20 + image_size) : 20)) + "px")
-                    .style("font-color", "white")
-                    .transition()
-                    .style("opacity", 0.9),
+        .attr("fill", function(d) { return d.label == "true" ? "black" : "green" })
+        .on("mouseover", 
+            function(d) {
+                tooltip.style("visibility", "visible")
             }
-        })
-        .on("mouseout", function(d) {
-            tooltip.style("visibility", "hidden")
-        })
+        )
+        .on("mousemove", 
+            function(d) {
+                var string = 
+                    "<img src=img/" + d.index + ".png style='height: " + image_size + 
+                    "px; width: " + image_size + "px'>"
+                return {
+                    image : tooltip.html(string)
+                        .style(
+                            "left", 
+                            (d3.event.pageX + 
+                                (d3.event.pageX > w / 2 ? -(10 + image_size) : 10)
+                            ) + "px"
+                        )
+                        .style("top", 
+                            (d3.event.pageY + 
+                                (d3.event.pageY > h / 2 ? -(20 + image_size) : 20)
+                            ) + "px"
+                        )
+                        .style("font-color", "white")
+                        .style("background-color", "white")
+                        .transition()
+                        .style("opacity", 0.9)
+                }
+            }
+        )
+        .on("mouseout", function(d) { tooltip.style("visibility", "hidden") })
 
     // image tooltip style attributes
     var tooltip = d3.select("body")
         .append("div")
-        .attr("width", 50 + 'px')
-        .attr("width", 50 + 'px')
         .style("position", "absolute")
-        .style("font-size", "10px")
-        .style("z-index", "10")
-        .style("visibility", "hidden")
 }
